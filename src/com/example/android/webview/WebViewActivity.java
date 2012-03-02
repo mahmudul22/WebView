4e41cd6aac27999d04ba90cbc281409557f206f0
mahmudul haque <mahmudul22@hotmail.com>
2012-03-02 15:55:52 +0100

    Initial commit

--- src/com/example/android/webview/WebViewActivity.java
+++ src/com/example/android/webview/WebViewActivity.java
@@ -2,6 +2,7 @@ package com.example.android.webview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -10,31 +11,13 @@ import android.widget.Toast;
 
 public class WebViewActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+    WebView myWebView;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        WebView myWebView = (WebView) findViewById(R.id.webView1);
+        myWebView = (WebView) findViewById(R.id.webView1);
         myWebView.setWebViewClient(new WebViewClient());
-        getWindow().requestFeature(Window.FEATURE_PROGRESS);
-
-        myWebView.getSettings().setJavaScriptEnabled(true);
-
-        final Activity activity = this;
-        myWebView.setWebChromeClient(new WebChromeClient() {
-          public void onProgressChanged(WebView view, int progress) {
-            // Activities and WebViews measure progress with different scales.
-            // The progress meter will automatically disappear when we reach 100%
-            activity.setProgress(progress * 1000);
-          }
-        });
-        myWebView.setWebViewClient(new WebViewClient() {
-          public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-          }
-        });
-
-        
         myWebView.loadUrl("http://129.242.19.250:3000/quiz/set 1");
     }
+
 }
