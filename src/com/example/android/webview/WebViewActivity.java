fb18a84afa674632a84f53f7e1b9d99f4731a562
mahmudul haque <mahmudul22@hotmail.com>
2012-03-02 15:26:42 +0100

    Building the repository with the webView for the first time

---
+++ src/com/example/android/webview/WebViewActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.webview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class WebViewActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        WebView myWebView = (WebView) findViewById(R.id.webView1);
+        myWebView.setWebViewClient(new WebViewClient());
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+        myWebView.setWebChromeClient(new WebChromeClient() {
+          public void onProgressChanged(WebView view, int progress) {
+            // Activities and WebViews measure progress with different scales.
+            // The progress meter will automatically disappear when we reach 100%
+            activity.setProgress(progress * 1000);
+          }
+        });
+        myWebView.setWebViewClient(new WebViewClient() {
+          public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+          }
+        });
+
+        
+        myWebView.loadUrl("http://129.242.19.250:3000/quiz/set 1");
+    }
+}
